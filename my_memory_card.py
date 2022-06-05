9aa43aa870dff6f05f316f37617423e5faa6350c
Longues <106917917+Longues@users.noreply.github.com>
2022-06-05 17:19:31 +0300

    Add files via upload
    оно существует

---
+++ my_memory_card.py
@@ -0,0 +1,81 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
+
+
+
+app = QApplication([])
+ 
+btn_OK = QPushButton('Вопросы?') 
+lb_Question = QLabel('Время тупых вопросов') 
+ 
+RadioGroupBox = QGroupBox("Варианты как получить в лицо") 
+ 
+rbtn_1 = QRadioButton('левая рука')
+rbtn_2 = QRadioButton('правая рука')
+rbtn_3 = QRadioButton('левая нога')
+rbtn_4 = QRadioButton('правая нога')
+ 
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+ 
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() 
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) 
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) 
+layout_ans3.addWidget(rbtn_4)
+ 
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3) 
+ 
+RadioGroupBox.setLayout(layout_ans1) 
+ 
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!') 
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignHCenter | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout() 
+layout_line3 = QHBoxLayout() 
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() 
+ 
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2)
+layout_line3.addStretch(1)
+ 
+layout_card = QVBoxLayout()
+ 
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) 
+
+ 
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+ 
+
+
+
+window.resize(400, 300)
+window.show()
+app.exec()
